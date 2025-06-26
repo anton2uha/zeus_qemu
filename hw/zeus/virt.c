50aff6ddb85b36f2e532a1b3ae550a032537f917
Igor Lesik <curoles@yahoo.com>
2025-06-25 21:09:12 -0700

    make zeus virt machine

---
+++ hw/zeus/virt.c
@@ -0,0 +1,94 @@
+#include "qemu/osdep.h"
+#include "qemu/units.h"
+#include "qapi/error.h"
+#include "hw/boards.h"
+#include "hw/qdev-core.h"
+#include "qemu/log.h"
+#include "hw/loader.h"
+#include "elf.h"
+#include "target/zeus/cpu.h"
+#include "hw/zeus/virt.h"
+
+static
+void zeus_load_program(ZeusVirtMachineState *board, const char *filename)
+{
+    g_assert_nonnull(filename);
+
+    hwaddr pentry G_GNUC_UNUSED = 0;
+#if 0
+    ssize_t prg_size G_GNUC_UNUSED = load_elf_ram(
+        /* filename     */ filename,
+        /* elf_note     */ NULL,
+        /* translate    */ NULL,
+        /* trans opaque */ NULL,
+        /* pentry       */ &pentry,
+        /* lowaddr      */ NULL,
+        /* highaddr     */ NULL,
+        /* pflags       */ NULL,
+        /* big endian   */ 0,
+        /* elf_machine  */ EM_NONE,
+        /* clear_lsb    */ 1,
+        /* data_swab    */ 0,
+        /* address space*/ address_space,
+        /* load_rom     */ false
+    );
+
+    g_assert(prg_size > 0);
+#endif
+g_assert_not_reached();
+    //qemu_log("Program %s loaded, size:%ld entry:%lx\n", filename, prg_size, pentry);
+}
+
+static void zeus_virt_machine_init(MachineState *machine)
+{
+    ZeusVirtMachineState *board = ZEUS_VIRT_MACHINE(machine);
+
+    uint64_t ram_size = 1 * GiB;
+
+    // User can request larger RAM size from command line
+    machine->ram_size = (ram_size > machine->ram_size)?
+        ram_size :
+        machine->ram_size;
+
+    memory_region_init_ram_nomigrate(
+        &board->dram,      // DRAM installed on the board
+        NULL,              // no owner
+        "dram",
+        machine->ram_size,
+        &error_abort
+    );
+    
+    memory_region_add_subregion(
+        get_system_memory(),
+        0x1000,
+        &board->dram
+    );
+    
+    if (machine->kernel_filename) {
+        zeus_load_program(board, machine->kernel_filename);
+    }
+}
+
+static void zeus_virt_machine_class_init(ObjectClass *oc, const void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(oc);
+
+    mc->desc             = "Zeus Virt Board";
+    mc->init             = zeus_virt_machine_init;
+    mc->min_cpus         = 1;
+    mc->max_cpus         = mc->min_cpus;
+    mc->default_cpus     = mc->min_cpus;
+    mc->default_cpu_type = ZEUS_CPU_TYPE_NAME("base");
+    mc->default_ram_size = 1 * GiB;
+}
+
+static const TypeInfo types[] = {
+    {
+        .name          = TYPE_ZEUS_VIRT_MACHINE,
+        .parent        = TYPE_MACHINE,
+        .class_init    = zeus_virt_machine_class_init,
+        .instance_size = sizeof(ZeusVirtMachineState),
+    },
+};
+
+DEFINE_TYPES(types);
---
+++ include/hw/zeus/virt.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "qemu/osdep.h"
+
+/* Machine name string.
+ *
+ * `$ qemu-system-zeus -M virt`
+ */
+#define TYPE_ZEUS_VIRT_MACHINE MACHINE_TYPE_NAME("virt")
+ 
+#define ZEUS_VIRT_MACHINE(obj) \
+    OBJECT_CHECK(ZeusVirtMachineState, (obj), TYPE_ZEUS_VIRT_MACHINE)
+
+/* Zeus Virt QEMU machine.
+ *
+ */
+typedef struct ZeusVirtMachineState {
+    MachineState parent_obj;
+
+    // DRAM installed on the board.
+    MemoryRegion dram;
+
+} ZeusVirtMachineState;
